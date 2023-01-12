bdba6bbf6a5424c5d63ecba8f42dfd90410ca958
mrvarma0 <96676649+mrvarma0@users.noreply.github.com>
2023-01-12 20:05:16 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const app = require("./app");
-// eslint-disable-next-line no-undef
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`server listening at port - ${port}`);
-});
